831663a730c479f502e38ef3d4929f03072693a0
Mike-newbie <durdan.first@gmail.com>
2023-01-24 15:50:10 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// let number = 7;
+// console.log(number); 
+
+// const obj = {
+//     a: 10
+// };
+
+// obj.a = 7;
+
+// console.log(obj);
+// const result = confirm("Do you confirm?");
+// console.log(result);
+
+// const answer = prompt("Are you 18?", "18");
+// console.log(answer);
+// console.log(typeof(answer));
+
+const category = "toys";
+
+console.log(`https://someurl.com/${category}/5` );
+
+const user ="Mike";
+
+alert(`Hi ${user}`);
